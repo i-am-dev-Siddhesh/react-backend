563488c579a06f65957a5ad7e81e100174f684b2
its-siddhesh <shindesiddhesh.co.in@gmail.com>
2022-10-07 20:56:30 +0530

    fix: script for build updated

--- dist/utils/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_RUNNING_MESSAGE = exports.GENERAL_ERROR_MESSAGE = exports.ACCESS_DENIED_MESSAGE = exports.__prod__ = void 0;
-exports.__prod__ = process.env.SERVER_ENV === "production";
-exports.ACCESS_DENIED_MESSAGE = "Access to the resource is denied", exports.GENERAL_ERROR_MESSAGE = "Something went wrong", exports.SERVER_RUNNING_MESSAGE = "Server is in running state";
